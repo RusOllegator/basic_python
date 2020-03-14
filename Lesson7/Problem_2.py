d4b3c91542d07ea7115b544120dc612106d7077c
Oleg Skorobogaty <oskorobogaty@gmail.com>
2020-03-14 03:37:11 +0300

    Первое задание 7го урока

---
+++ Lesson7/Problem_2.py
@@ -0,0 +1,8 @@
+class MyClass :
+    def __init__ (self, param):
+        self.param = param
+    def __del__ (self):
+        print( f"Удаляем объект {self.param} класса MyClass" )
+
+mc = MyClass( "text" )
+del mc
